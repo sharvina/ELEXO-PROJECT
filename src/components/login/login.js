a982c184d2cccecb73ce02a2a4d93a6847de9820
sharvina vs <sharvina.s@zerone-consulting.com>
2025-09-26 18:31:23 +0530

    signup page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,43 +1,13 @@
 import React, { useState } from 'react';
 
-function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle login logic here
-        console.log('Email:', email, 'Password:', password);
-    };
+function Login() {
+
 
     return (
         <div style={{ maxWidth: 400, margin: 'auto', padding: 20 }}>
             <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div style={{ marginBottom: 10 }}>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        required
-                        style={{ width: '100%', padding: 8 }}
-                    />
-                </div>
-                <div style={{ marginBottom: 10 }}>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        required
-                        style={{ width: '100%', padding: 8 }}
-                    />
-                </div>
-                <button type="submit" style={{ width: '100%', padding: 10 }}>
-                    Login
-                </button>
-            </form>
+
         </div>
     );
 }
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import Header from '../header/header';
-import signupImg from '../../images/signup-img.png';
+import signupImg from '../../images/signup.jpg';
 import './signup.css';
 import '../../styles/global.css';
 import googleImg from '../../images/google.svg';
+import { Link } from "react-router-dom";
+
+
 
 function Signup() {
     const handleGoogleLogin = () => {
@@ -31,13 +34,11 @@ function Signup() {
 
     return (
         <div >
-            <Header />
+
             <div className='container mx-auto py-5'>
                 <div className='signup_container d-flex  align-items-center' >
-                    <div className='left_side'>
-                        <img src={signupImg} alt='signup img'></img>
-                    </div>
-                    <div className='right_side'>
+
+                    <div className='form_section'>
                         <h2>Create an account</h2>
                         <p>Enter your details below</p>
                         <form onSubmit={handleSubmit}>
@@ -74,12 +75,16 @@ function Signup() {
                                     onChange={handleChange}
                                 />
                             </div>
-                            <div className='form_footer mt-4'>
+                            <div className='form_footer mt-5'>
                                 <button className='btn_tag btn_primary w-100' type="submit">Sign Up</button>
                                 <button className='d-flex justify-content-center gap-3 align-items-center  mt-3 btn_tag  w-100 ' type="submit" onClick={handleGoogleLogin}><img src={googleImg} alt='google icon'></img>Sign up with Google</button>
-                                <a className='pt-4 text-center d-block'>Already have account? Log in</a>
+                                <a className='pt-4 text-center d-block'>Already have account? <Link to="/login"> Log in</Link></a>
                             </div>
                         </form>
+                    </div>
+                    <div className='banner_section'>
+                        <img src={signupImg} alt='signup img'></img>
+                        <h2>Exclusive Discounts Await – <br />  Sign Up Free!</h2>
                     </div>
                 </div>
             </div>
